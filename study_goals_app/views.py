0bc689149a884d345404ccdec5057f4ed6e2dc68
benngoyi1 <tshibola.ngoyi@ndsu.edu>
2023-04-22 13:33:56 -0500

    Transfered content

--- study_goals_app/views.py
+++ study_goals_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from study_goals_app.models import Study_Goal, Study_Goal_Attachment,Related_Task
 
 # Create your views here.
+
+def Study_goals(request):
+    dict = {'inser_here': 'this is views.py', 'vSG':'a', 'VSGD': 'b', 'ESG': 'c', 'NSG': 'd'}
+    return HttpResponse("Hello from Study goals")
+
+    return render(request, 'study_goals_app/study_goals.html', context=dict)
