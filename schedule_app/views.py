436d8d4ff89914a9c4919d2861a92b77eb7e6824
benngoyi1 <tshibola.ngoyi@ndsu.edu>
2023-04-22 15:51:56 -0500

    added views.py and urls.py to schedule app

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 from schedule_app.models import Event 
 
 # Create your views here.
+
+def Schedule(request):
+    events_list = Event.objects.order_by('date')
+    event_dict ={'event': events_list}
+    
+    return render(request, 'schedule_app/schedule.html', context = event_dict)
